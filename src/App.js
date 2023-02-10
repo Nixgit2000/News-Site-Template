0d936c72262af05cff11def7fe96485ff4441aba
Nixgit2000 <fourwaycoordination@gmail.com>
2023-02-10 13:20:19 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function Header() {
+return (
+<>
+<div id="spacer1">.</div>
+ <div id="header-container"><div id="header">THE DAILY NEWS</div></div>
+
+<div id="grid">
+<div id="button1">WORLD NEWS</div><div id="button2">FINANCE</div><div id="button3">POLITICS</div><div id="button4">ECONOMICS</div><div id="button5">BUSINESS</div>
+</div>
+
+<div class="container">
+<div id="pic4"></div>
+<div id="pic3"></div>
+<div id="pic2"></div>
+<div id="pic2"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic4"></div>
+<div id="pic"></div>
+<div id="pic3"></div>
+<div id="pic2"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic4"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic4"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic4"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic3"></div>
+<div id="pic4"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic4"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic4"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic4"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic4"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic4"></div>
+<div id="pic"></div>
+<div id="pic"></div>
+<div id="pic3"></div>
+<div id="pic3"></div>
+<div id="pic"></div>
+<div id="pic2"></div>
+<div id="pic"></div>
+</div>
+<div id="spacer-container"><div id="spacer">Copyright 2023 Nixgit2000</div></div>
+</>
+);
 }
 
-export default App;
